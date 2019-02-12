4df015e84f007d83392b5da57749df5a046780a8
Zhenhao <zzh2531512@gmail.com>
2019-02-12 16:46:59 -0500

    Implement the function.

--- BeerSong/BeerSong/BeerSong.cpp
+++ BeerSong/BeerSong/BeerSong.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 
-void Ninety_nine_bottles_of_beer() {}
+void Ninety_nine_bottles_of_beer() {
+	for (int i = 99; i > 1; i--) {
+		cout << i << " bottles of beer on the wall, " << i << " bottles of beer" << endl;
+		cout << "take one down, pass it around, " << i - 1 << " bottles of beer on the wall." << endl;
+	}
+	cout << "1 bottle of beer on the wall, 1 bottle of beer" << endl;
+	cout << "take one down, pass it around, no more bottles of beer on the wall. " << endl;
+}
 
-int main() {}
+int main() {
+	Ninety_nine_bottles_of_beer();
+	system("pause");
+	return 0;
+}
